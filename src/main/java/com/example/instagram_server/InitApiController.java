973d2b307e1490ecc0b4cf6a0e438ec8fa26d969
devlhj <devlhj@naver.com>
2025-10-02 06:46:10 +0900

    first commit

---
+++ src/main/java/com/example/instagram_server/InitApiController.java
@@ -0,0 +1,14 @@
+package com.example.instagram_server;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class InitApiController {
+
+    @GetMapping("/")
+    public String init() {
+        return "Hello World";
+    }
+
+}
