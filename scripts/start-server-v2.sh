771f7023751dc426661d3d9bb5537df61ec209ae
devlhj <devlhj@naver.com>
2025-10-19 23:19:13 +0900

    fix final codedeploy 2

--- scripts/start-server-v2.sh
+++ scripts/start-server-v2.sh
@@ -7,9 +7,9 @@ docker stop instagram-server || true
 # docker container 삭제
 docker rm instamgram-server || true
 # docker image pull
-docker pull
+docker pull 776036137161.dkr.ecr.ap-northeast-2.amazonaws.com/instagram-server:latest
 # docker run
-docker run -d -p 8080:8080 --name instagram-server
+docker run -d -p 8080:8080 --name instagram-server 776036137161.dkr.ecr.ap-northeast-2.amazonaws.com/instagram-server:latest
 
 
 echo "----------------배포 끝--------------------------"
